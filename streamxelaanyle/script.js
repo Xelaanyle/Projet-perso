ef95f86eb0cd0c4d8a02c553dd14d1762f82205e
Alexandre <Alechemin@gmail.com>
2023-07-12 15:14:35 +0200

    Update

--- streamxelaanyle/script.js
+++ streamxelaanyle/script.js
@@ -1,18 +1,26 @@
 function toggleIconVisibility() {
-    var iconlist = document.getElementById("iconlist");
-    iconlist.style.display = "none";
+  var iconlist = document.getElementById("iconlist");
+  iconlist.style.display = "none";
 }
 
 var circle = document.querySelector(".circle");
 circle.addEventListener("click", toggleIconVisibility);
 
-window.addEventListener('load', function() {
-    var header = document.querySelector('.header');
-    header.classList.add('header-appear');
-  
-    setTimeout(function() {
-      var iconList = document.querySelector('.icon');
-      iconList.classList.add('icon-appear');
-    }, 5000); // 30 secondes en millisecondes
-  });
-  
+window.addEventListener('load', function () {
+  var header = document.querySelector('.header');
+  header.classList.add('header-appear');
+
+  setTimeout(function () {
+    var iconList = document.querySelector('.icon');
+    iconList.classList.add('icon-appear');
+  }, 5000); // 30 secondes en millisecondes
+});
+
+function openPopup() {
+  window.open('http://localhost/traitement.php', '_blank', 'width=900,height=180');
+
+  var myForm = document.getElementById('myForm');
+    myForm.reset();
+
+  return false;
+}
